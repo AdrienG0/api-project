17f33e947ee9dd8b43052e42df3d0a648a891f8c
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2024-12-26 13:04:15 +0100

    Nieuwsberichten implementeren

--- server.js
+++ server.js
@@ -115,6 +115,96 @@ app.delete('/users/:id', (req, res) => {
 });
 
 
+//Lijst ophalen van nieuwsberichten
+
+app.get('/newsposts', (req, res) => {
+  const query = 'SELECT * FROM newsposts';
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Er is een fout opgetreden');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+//Haal een nieuwsbericht op basis van id
+
+app.get('/newsposts/:id', (req, res) => {
+  const postId = req.params.id;
+  const query = 'SELECT * FROM newsposts WHERE id = ?';
+  db.query(query, [postId], (err, results) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Er is een fout opgetreden');
+    } else if (results.length === 0) {
+      res.status(404).send('Nieuwbericht niet gevonden');
+    } else {
+      res.json(results[0]);
+    }
+  });
+});
+
+//Voeg een nieuwsbericht toe
+app.post('/newsposts', (req, res) => {
+  const { title, content, author } = req.body;
+
+  if (!title || !content || !author) {
+    return res.status(400).send('Alle velden zijn verplicht');
+  }
+
+  const query = 'INSERT INTO newsposts (title, content, author) VALUES (?, ?, ?)';
+  db.query(query, [title, content, author], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Er is een fout opgetreden');
+    } else {
+      res.status(201).send('Nieuwbericht succesvol toegevoegd');
+    }
+  });
+});
+
+//Bestaand Nieuwsbericht bewerken
+app.put('/newsposts/:id', (req, res) => {
+  const postId = req.params.id;
+  const { title, content, author } = req.body;
+
+  if (!title || !content || !author) {
+    return res.status(400).send('Alle velden zijn verplicht');
+  }
+
+  const query = 'UPDATE newsposts SET title = ?, content = ?, author = ? WHERE id = ?';
+  db.query(query, [title, content, author, postId], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Er is een fout opgetreden');
+    } else if (result.affectedRows === 0) {
+      res.status(404).send('Nieuwbericht niet gevonden');
+    } else {
+      res.send('Nieuwbericht succesvol bijgewerkt');
+    }
+  });
+});
+
+//verwijder een bestaand nieuwsbericht
+app.delete('/newsposts/:id', (req, res) => {
+  const postId = req.params.id;
+  const query = 'DELETE FROM newsposts WHERE id = ?';
+  db.query(query, [postId], (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Er is een fout opgetreden');
+    } else if (result.affectedRows === 0) {
+      res.status(404).send('Nieuwbericht niet gevonden');
+    } else {
+      res.send('Nieuwbericht succesvol verwijderd');
+    }
+  });
+});
+
+
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server draait op http://localhost:${PORT}`);
